a621d5161b8ffa4a03e7c4ecda3c3501aec56d45
mkcetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2022-05-18 12:23:56 +0300

    Update submariner.go

--- pkg/deploy/submariner.go
+++ pkg/deploy/submariner.go
@@ -42,7 +42,7 @@ type SubmarinerOptions struct {
 	LoadBalancerEnabled           bool
 	HealthCheckEnabled            bool
 	NATTPort                      int
-	NatDiscovery 		      uint
+	NatDiscovery 		      int
 	HealthCheckInterval           uint64
 	HealthCheckMaxPacketLossCount uint64
 	ClusterID                     string
